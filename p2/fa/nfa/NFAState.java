4ca4d9031a0c729308d1c3d613dc6b85991e066b
Trevor Smith <trevorsmith772@u.boisestate.edu>
2021-04-07 20:50:23 -0600

    Added transitions variable

--- p2/fa/nfa/NFAState.java
+++ p2/fa/nfa/NFAState.java
@@ -1,9 +1,15 @@
 package fa.nfa;
 
+import java.util.LinkedHashMap;
+import java.util.Set;
+
 import fa.State;
 
 public class NFAState extends State{
     
+    /* Instance Variables */
+    private LinkedHashMap<Character, Set<NFAState>> transitions; //delta
+
     public NFAState(String name){
         this.name = name;
     }
