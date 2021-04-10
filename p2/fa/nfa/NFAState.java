7858c4c1a3ea8d3bcdc293530277c50d057f0a58
Trevor Smith <trevorsmith772@u.boisestate.edu>
2021-04-10 10:44:25 -0600

    Added to NFAState

--- p2/fa/nfa/NFAState.java
+++ p2/fa/nfa/NFAState.java
@@ -9,9 +9,12 @@ public class NFAState extends State{
     
     /* Instance Variables */
     private LinkedHashMap<Character, Set<NFAState>> transitions; //delta
+    private boolean isFinal; //boolean for if the state is final
 
     public NFAState(String name){
         this.name = name;
+        transitions = new LinkedHashMap<Character, Set<NFAState>>();
+        isFinal = false;
     }
 
 }
