4280f8d58dbc380ce05d8453e9cb283636863e5c
Trevor Smith <trevorsmith772@u.boisestate.edu>
2021-04-10 15:23:51 -0600

    Added javadoc comments for all methods

--- p2/fa/nfa/NFA.java
+++ p2/fa/nfa/NFA.java
@@ -20,7 +20,12 @@ public class NFA implements NFAInterface{
         initialState = null;
     }
 
-    @Override
+    /**
+     * Adds a start state to the NFA.
+     *  Checks if the start already exists
+     *  first.
+     * @param name - the name of the start state
+     */
     public void addStartState(String name) {
         NFAState s = checkIfExists(name);
 		if(s == null){
@@ -30,7 +35,11 @@ public class NFA implements NFAInterface{
 		initialState = s;
     }
 
-    @Override
+    /**
+     * Adds a state to the NFA
+     * @param name - the name of the state
+     *              being added
+     */
     public void addState(String name) {
         nfaStates.add(new NFAState(name));
     }
@@ -47,7 +56,12 @@ public class NFA implements NFAInterface{
         
     }
 
-    @Override
+    /**
+     * Adds a transition to the NFA
+     * @param fromState - the state the transition is from
+     * @param onSymb - the alphabet character the transition occurs on
+     * @param toState - the state being transitioned to
+     */
     public void addTransition(String fromState, char onSymb, String toState) {
         NFAState from = checkIfExists(fromState);
         NFAState to = checkIfExists(toState);
@@ -58,16 +72,21 @@ public class NFA implements NFAInterface{
             System.err.println("ERROR: No DFA state exists with name " + toState);
             System.exit(2);
         }
-
-        
+        //TODO
     }
 
-    @Override
+    /**
+     * Getter for the states set
+     * @return Set of NFA states
+     */
     public Set<? extends State> getStates() {
         return nfaStates;
     }
 
-    @Override
+    /**
+     * Getter for final states
+     * @return Set of NFA final states
+     */
     public Set<? extends State> getFinalStates() {
         LinkedHashSet<NFAState> finalStates = new LinkedHashSet<>();
         for(NFAState state : nfaStates){
@@ -78,29 +97,53 @@ public class NFA implements NFAInterface{
         return finalStates;
     }
 
-    @Override
+    /**
+     * Getter for start state
+     * @return the start state for the NFA
+     */
     public State getStartState() {
         return initialState;
     }
 
-    @Override
+    /**
+     * Getter for NFA alphabet
+     * @return the alphabet for the NFA
+     */
     public Set<Character> getABC() {
         return alphabet;
     }
 
-    @Override
+    /**
+     * Computes an equivalent DFA for
+     * the given NFA and returns it.
+     * @return an equivalent DFA
+     */
     public DFA getDFA() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /**
+     * Returns states that will be
+     * transitioned to from a given 
+     * state and alphabet character
+     * @param from - the state being transitioned from
+     * @param onSymb - the alphabet symbol being transitioned on
+     * @return a set of states that will be transitioned to
+     *  from the state on the character
+     */
     public Set<NFAState> getToState(NFAState from, char onSymb) {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /**
+     * Searches for states that can be reached
+     *  from the start state with an empty string
+     * @param s - the start state for the NFA
+     * @return a set of NFA states that can be
+     *  reached from the start state with epsilon
+     */
     public Set<NFAState> eClosure(NFAState s) {
         // TODO Auto-generated method stub
         return null;
